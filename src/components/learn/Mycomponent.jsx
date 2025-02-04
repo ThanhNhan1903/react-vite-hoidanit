79a02367d1f888ecf6bbeb56daec4fbd1cf10fb2
unknown <nhan.tt1903@gmail.com>
2025-02-04 22:10:41 +0700

    video 30

--- src/components/learn/Mycomponent.jsx
+++ src/components/learn/Mycomponent.jsx
@@ -1,5 +1,15 @@
 //Component = html + css + js
+//JSX: 1 prarent
+//fragment
+import "./style.css";
 const MyComponent = () => {
-  return <div>eric & hoidanit update</div>;
+  return (
+    <>
+      <div>eric & hoidanit update</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        child
+      </div>
+    </>
+  );
 };
 export default MyComponent;
