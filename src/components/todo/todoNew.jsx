4a5d3e9b59a6ecabb5d843a412f6356fc48ff165
unknown <nhan.tt1903@gmail.com>
2025-02-05 21:30:43 +0700

    video 36

--- src/components/todo/todoNew.jsx
+++ src/components/todo/todoNew.jsx
@@ -2,10 +2,24 @@ const TodoNew = (props) => {
   // console.log("check props2: ", props);
   const { addNewTodo } = props;
   // addNewTodo("eric");
+  const handleClick = () => {
+    alert("click me");
+  };
+  const handleOnchange = (event) => {
+    console.log(event);
+  };
   return (
     <div className="todo-new">
-      <input type="text" placeholder="Enter your task" />
-      <button>Add</button>
+      <input
+        type="text"
+        placeholder="Enter your task"
+        onChange={(event) => {
+          handleOnchange(event.target.value);
+        }}
+      />
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 };
