31b7bcde95e44a648a6ed3fa170b414c9ef2e751
unknown <nhan.tt1903@gmail.com>
2025-02-05 13:39:03 +0700

    video 33

---
+++ src/components/todo/todoData.jsx
@@ -0,0 +1,9 @@
+const TodoData = () => {
+  return (
+    <div className="todo-data">
+      <div>Learning english</div>
+      <div>Learning chinese</div>
+    </div>
+  );
+};
+export default TodoData;
