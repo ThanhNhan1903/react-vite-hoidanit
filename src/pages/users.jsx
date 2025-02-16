1697e75122f225ea4a5e1524653b8317776cb3c5
unknown <nhan.tt1903@gmail.com>
2025-02-16 20:06:44 +0700

    video 66

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,6 +1,12 @@
+import UserForm from "../components/users/user.form";
+import UserTable from "../components/users/users.table";
+
 const UsersPage = () => {
     return (
-        <div>users page</div>
+        <div style={{ padding: "20px" }}>
+            <UserForm />
+            <UserTable />
+        </div>
     )
 };
 export default UsersPage;
