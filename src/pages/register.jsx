5a6cd8a309c36435911390417ffd7d9898730b06
ThanhNhan1903 <nhan.tt1903@gmail.com>
2025-03-02 15:31:42 +0700

    video 90

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,9 +1,31 @@
-import { Button, Form, Input, InputNumber } from "antd";
+import { Button, Form, Input, InputNumber, notification } from "antd";
+import { registerUserAPI } from "../services/api.services";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
     const [form] = Form.useForm();
-    const onFinish = (values) => {
+    const navigate = useNavigate()
+    const onFinish = async (values) => {
         console.log(values)
+
+        const res = await registerUserAPI(
+            values.fullName,
+            values.email,
+            values.password,
+            values.phone);
+        if (res.data) {
+            notification.success({
+                message: "Register User",
+                description: "Register thanh cong"
+            });
+            navigate("/login")
+        } else {
+            notification.error({
+                message: "Register user",
+                description: JSON.stringify(res.message)
+            })
+        }
+
     }
     return (
         <Form
@@ -19,53 +41,56 @@ const RegisterPage = () => {
                 <Form.Item
                     label="Full Name"
                     name="fullName"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
                 <Form.Item
                     label="Email"
-                    name="Email"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Email!',
+                        },
+                    ]}
                 >
                     <Input />
                 </Form.Item>
                 <Form.Item
                     label="Password"
                     name="password"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
                 >
                     <Input.Password />
                 </Form.Item>
                 <Form.Item
                     label="Phone Number"
                     name="phone"
-                // rules={[
-                //     {
-                //         required: true,
-                //         message: 'Please input your username!',
-                //     },
-                // ]}
+                    rules={[
+                        {
+                            required: true,
+                            pattern: new RegExp(/\d+/g),
+                            message: "Wrong format!"
+                        }
+
+                    ]}
                 >
                     <Input />
                 </Form.Item>
-                <div><Button onClick={() => form.submit()} type="primary">Register</Button></div>
-                {/* <button type="submit">Register</button> */}
+                <div>
+                    <Button onClick={() => form.submit()} type="primary">Register</Button>
+                </div>
 
             </div>
 
